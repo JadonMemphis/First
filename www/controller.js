b3d6aba055c7c7a32b59d751336216413f128f14
JadonMemphis <jadon.memphis@gmail.com>
2015-04-17 16:01:40 -0400

    update first

--- www/controller.js
+++ www/controller.js
@@ -1,7 +1,57 @@
 var app = new function(app){
 
-	app.makeHotSpots = function(){
+	app.makeHotSpots = function(assets, pages, preload){
 		zog("hotspots");
+		var hs = new zim.HotSpots([
+			{page:assets.main, rect:assets.main.button, call:function(){
+				zog("we here");
+				pages.go(assets.info, "right");
+
+				createjs.Ticker.addEventListener("tick",updateGame);
+				createjs.Ticker.setFPS(30);
+
+
+
+			}},
+			{page:assets.main, rect:[0,0,260,260], call:function(){pages.go(assets.info, "right");}}
+		]);
+		var finished = false;
+		var destroyed = 0;
+		var total = assets.info.moonies.numChildren;
+		assets.info.moonies.on("click", function(e){
+			var blowUp = new jadon.Blow(assets.info.moonies, e.target, preload.getResult("explode"));
+			destroyed++;
+	
+			if (destroyed >= total) {
+				gameOver("success");
+			}
+		});
+		
+		var t = 0;
+		
+
+		function updateGame(){
+			if (!finished) {
+				if (assets.info.counter .text >= 60) {
+					gameOver("fail");
+				}
+				t++;
+				assets.info.counter.text = Math.floor(t/30);
+			}
+			stage.update();
+		}
+
+		function gameOver(status){
+			finished = true;
+			//createjs.Ticker.removeEventListener("tick",updateGame);
+			if(status == "fail"){
+				console.log("you failed, you missed " + (total - destroyed + " moons"));
+			} else if( status == "success"){
+				console.log("Congrats you finished in " + assets.info.counter.text + "s.");
+			}
+		}
+
+		
 	}
 	return app;
 }(app||{});
--- www/view.js
+++ www/view.js
@@ -4,68 +4,80 @@ zog("hi from view.js");
 
 var app = function(app) {
 	
-	app.makeVerticalPages = function(layoutManager, gridManager, guideManager) {
+	app.makeVerticalPages = function(layoutManager, gridManager, guideManager, preload) {
 		
-		zog("pages");
 		
 		p = {};
 		
 		p.main = new createjs.Container();		
 		p.main.name = "main";	
-		p.main.setBounds(0,0,stageW,stageH);
+		p.main.setBounds(0,0,stageW,stageH);		
 		
-		var logo = new zim.Rectangle(100, 100, "orange");	
-		logo.setBounds(0,0,100,100);
-		p.main.addChild(logo);
-		
-		var content = p.main.content = new createjs.Container();
-		content.setBounds(0,0,600,600);
-		var thing = p.mainThing = new zim.Rectangle(200,200,"red");
-		thing.x = 100;
-		thing.y = 100;		
-		content.addChild(thing);
-
-		var thing1 = p.mainThing1 = new zim.Rectangle(200,200,"blue");
-		thing1.x = 100;
-		thing1.y = 300;		
-		content.addChild(thing1);
-
-		var thing2 = p.mainThing2 = new zim.Rectangle(200,200,"blue");
-		thing1.x = 100;
-		thing1.y = 300;		
-		content.addChild(thing1);
-
-		p.main.addChild(content);
-
+		var introBack = new zim.Rectangle(stageW, stageH, "white");
+		p.main.addChild(introBack);
 
 		
-		var related = new zim.Rectangle(500, 200, "pink");	
-		related.setBounds(0,0,500,200);
-		p.main.addChild(related);
+		var button = p.main.button = new zim.Button(120,60,"Enter");
+		zim.centerReg(button);
+		button.x = stageW/2;
+		button.y = stageH/2;
+		//button.on("click", function() {
+
+		//});
+		p.main.addChild(button);
 		
-		
-		gridManager.add( new zim.Grid(content) );
+		/*gridManager.add( new zim.Grid(content) );
 		guideManager.add( new zim.Guide(content) );
 		
-		zog(layoutManager);
 		
-		var mainParts =[ {object:logo, marginTop:10, maxWidth:80, minHeight:20, align:"left", valign:"top"},
-							{object:content, marginTop:5, maxWidth:90, backgroundColor:"yellow"}, 
-							{object:related, marginTop:5, maxWidth:80, height:20, backgroundColor:"red"} ];
 		
-		var mainLayout = new zim.Layout(p.main, mainParts, 5, "black", true, new createjs.Shape(), stage);
+		var mainParts =[ {object:logo, marginTop:0, maxWidth:100, height:10, align:"left", valign:"top"},
+							{object:content, marginTop:0, maxWidth:100, backgroundColor:"yellow"}, 
+							{object:related, marginTop:10, maxWidth:100, height:10, backgroundColor:"red"} ];
+		
+		var mainLayout = new zim.Layout(p.main, mainParts, 5, null, true, new createjs.Shape(), stage);
 		
 		layoutManager.add(mainLayout);	
-		
+		*/
 		
 		
 		
 		p.info = new createjs.Container();		
-		p.info.name = "info";		
-		var infoBacking = new zim.Rectangle(stageW, stageH, "yellow");	
-		infoBacking.setBounds(0,0,stageW,stageH);
-		p.info.addChild(infoBacking);
-				
+		p.info.name = "info";
+		p.info.setBounds(0,0,stageW,stageH);
+
+		var back = p.info.back = new createjs.Bitmap(preload.getResult("back"));
+		//zog(back.getBounds());
+		back.x = 0;
+		back.y = 0;
+		p.info.addChild(back);
+		zim.scaleTo(back, stage, 100, 100, "both");
+
+		var counter =p.info.counter = new createjs.Text();
+		counter.x = stageW / 2;
+		counter.y = 50;
+		counter.id = counter;
+		p.info.addChild(counter);
+			
+		var moonies = p.info.moonies = new createjs.Container();
+		p.info.addChild(moonies);
+
+		var moony;
+		
+		var total = 10;
+		for(var i = 0; i<total; i++ ){
+			moony = new jadon.Moon(45);
+			moonies.addChild(moony);
+		} 
+
+
+		// keep at end of main function
+		// for the fit scale mode you will probably need to code below
+		// to scale things as the screen size varies	
+			
+		
+
+
 		return p;
 		
 	}
